cc53391fdfe1763fffd2f779863a90e006d3edd5
Paodpdro <126562990+Paodpdro@users.noreply.github.com>
2026-07-12 21:58:04 +0300

    Make the operation buttons working, basic two values operations working. Need to fix multiple values and multiple operations

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 
 function add(num1, num2) {
-    return num1+num2;
+    return parseInt(num1)+parseInt(num2);
 } 
 
 function subtract(num1, num2) {
-    return num1-num2;
+    return parseInt(num1)-parseInt(num2);
 }
 
 function multiply(num1, num2) {
-    return num1*num2;
+    return parseInt(num1)*parseInt(num2);
 }
 
 function divide(num1, num2) {
-    return num1/num2;
+    return parseInt(num1)/parseInt(num2);
 }
 
 function operate(operationSymbol, num1, num2) {
     let result;
-    if (operationSymbol === add) {
+    if (operationSymbol === "+") {
         result = add(num1, num2);
     }
-    else if (operationSymbol === subtract) {
+    else if (operationSymbol === "-") {
         result = subtract(num1, num2);
     }
-    else if (operationSymbol === multiply) {
+    else if (operationSymbol === "*") {
         result = multiply(num1, num2);
     }
     else {
@@ -32,16 +32,41 @@ function operate(operationSymbol, num1, num2) {
     return result;
 }
 
-let num1;
-let operationSymbol;
-let num2;
+function clickedDigit(number, symbol) {
+    if (symbol === '') {
+        num1 += `${number}`;
+        console.log(num1, "num1")
+        displayText.textContent = num1;
+    }
+    else {
+        num2 += `${number}`;
+        console.log(num2, "num2", num1, "num1")
+        displayText.textContent = num2;
+    }
+}
+
+let num1 = '';
+let operationSymbol ='';
+let num2 ='';
 
 const displayText = document.querySelector("#display");
 const digitButtons = document.querySelectorAll(".digits");
+const operationButtons = document.querySelectorAll(".operation");
+const equals = document.querySelector("#equals-button");
 
 digitButtons.forEach((button) => {
+    button.addEventListener("click", () => clickedDigit(button.textContent, operationSymbol))
+})
+
+operationButtons.forEach((button) => {
     button.addEventListener("click", () => {
-        displayText.textContent = button.textContent;
+        operationSymbol = button.textContent;
+        displayText.textContent = "";
     })
 })
 
+
+equals.addEventListener("click", () => {
+    console.log(operationSymbol, num1, num2)
+    displayText.textContent = operate(operationSymbol, num1, num2);
+})
